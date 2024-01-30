18ccedfc09a6701a1ebcb2af739956f7935c3d77
nereazj544 <nzjvirtual@gmail.com>
2024-01-30 21:23:10 +0100

    Piedra, papel, tijeras (Parte Logica)

--- src/app/componentes/ppt/ppt.component.ts
+++ src/app/componentes/ppt/ppt.component.ts
@@ -8,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class PptComponent implements OnInit {
   
   texto:string="";
+  texto2:string="";
+  texto3:string="";
+  
+  eleccionesDis:string[]=["piedra", "papel", "tijeras"];
 
   ngOnInit(): void {
     this.texto = "¡PIEDRA, PAPEL O TIJERA! ELIGE TU ELECCION"
+  }
+
+  elegir(eleccion:string){
+    this.texto = "Elegiste: " + eleccion;
+
+    var eleccionM=Math.floor(Math.random()*3); //se multipilica por 3 y se optiene un numero entre cero y tres (con decimales). 
+
+    this.eleccionesDis[eleccionM];
+    this.texto2 = "La maquina ha elegido: " + this.eleccionesDis [eleccionM];
+
+    if ((eleccion == "tijeras" && this.eleccionesDis[eleccionM]=="papel") || 
+    (eleccion == "papel" && this.eleccionesDis[eleccionM] == "piedra") 
+    || (eleccion == "piedra"  && this.eleccionesDis[eleccionM] == "tijeras") ){
+      this.texto3= "GANASTE";
+    }else if ((eleccion == "tijeras" && this.eleccionesDis[eleccionM]=="piedra") || 
+    (eleccion == "papel" && this.eleccionesDis[eleccionM] == "tijeras") 
+    || (eleccion == "piedra"  && this.eleccionesDis[eleccionM] == "papel") ) {
+      this.texto3="PIERDES";
+    }
+    else{
+      this.texto3="EMPATE";
+    }
   }
 
 }
