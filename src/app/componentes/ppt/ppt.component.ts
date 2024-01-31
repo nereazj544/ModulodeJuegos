8651a96a41bc89ae0d57b2151aa1c72be0235a25
nereazj544 <nzjvirtual@gmail.com>
2024-01-31 01:19:22 +0100

    Modificaciones en el juego: Piedra, papel o tijeras

--- src/app/componentes/ppt/ppt.component.ts
+++ src/app/componentes/ppt/ppt.component.ts
@@ -10,6 +10,9 @@ export class PptComponent implements OnInit {
   texto: string = "";
   texto2: string = "";
   texto3: string = "";
+  img1: string = "";
+  img2: string = "";
+  img3: string = "";
 
   eleccionesDis: string[] = ["piedra", "papel", "tijeras"];
 
@@ -18,9 +21,16 @@ export class PptComponent implements OnInit {
   }
 
   elegir(eleccion: string) {
+
+    //!Reseteo de imagenes
+    this.img1 = "";
+    this.img2 = "";
+    this.img3 = "";
+
     this.texto = "Elegiste: " + eleccion;
 
-    var eleccionM = Math.floor(Math.random() * 3); //se multipilica por 3 y se optiene un numero entre cero y tres (con decimales). 
+    var eleccionM = Math.floor(Math.random() * 3); 
+    //!Sin floor se quedan como decimales, con el floor se redondea a cero y a tres. 
 
     this.eleccionesDis[eleccionM];
     this.texto2 = "La maquina ha elegido: " + this.eleccionesDis[eleccionM];
@@ -28,15 +38,60 @@ export class PptComponent implements OnInit {
     if ((eleccion == "tijeras" && this.eleccionesDis[eleccionM] == "papel") ||
       (eleccion == "papel" && this.eleccionesDis[eleccionM] == "piedra")
       || (eleccion == "piedra" && this.eleccionesDis[eleccionM] == "tijeras")) {
-      this.texto3 = "GANASTE";
+      this.img1 = '/../../assets/img/GANAS.jpg';
+      //!Esto hara que se muestre la imagen de que has ganado.
     } else if ((eleccion == "tijeras" && this.eleccionesDis[eleccionM] == "piedra") ||
-      (eleccion == "papel" && this.eleccionesDis[eleccionM] == "tijeras")
-      || (eleccion == "piedra" && this.eleccionesDis[eleccionM] == "papel")) {
-      this.texto3 = "PIERDES";
+    (eleccion == "papel" && this.eleccionesDis[eleccionM] == "tijeras")
+    || (eleccion == "piedra" && this.eleccionesDis[eleccionM] == "papel")) {
+      // this.texto3 = "PIERDES";
+      this.img2 = '/../../assets/img/PIERDES.png';
+      //!Esto hara que se muestre la imagen de que has perdido.
     }
     else {
-      this.texto3 = "EMPATE";
+      this.img3 = '/../../assets/img/Empate.png';
+      //!Esto hara que se muestre la imagen de que has empatado.
+      
     }
   }
+
+
+
+  
+  /**  
+   * !CLASE
+   * 
+   * 
+   * texto: string = "";
+   * texto2: string = "";
+   * texto3: string = ""; 
+   * eleccionesDis: string[] = ["piedra", "papel", "tijeras"];
+   * ngOnInit(): void {
+   *     this.texto = "¡PIEDRA, PAPEL O TIJERA! ELIGE TU ELECCION"
+   *   }
+   *   elegir(eleccion: string) {
+   *     this.texto = "Elegiste: " + eleccion;
+   *     var eleccionM = Math.floor(Math.random() * 3); //se multipilica por 3 y se optiene un numero entre cero y tres (con decimales). 
+   *     this.eleccionesDis[eleccionM];
+   *    this.texto2 = "La maquina ha elegido: " + 
+   *    this.eleccionesDis[eleccionM];
+   *     if ((eleccion == "tijeras" && this.
+   * eleccionesDis[eleccionM] == "papel") ||
+   * (eleccion == "papel" && this.eleccionesDis
+   * [eleccionM] == "piedra")
+   *  || (eleccion == "piedra" && this.eleccionesDis
+   * [eleccionM] == "tijeras")) {
+   *         this.texto3 = "GANASTE";
+   *       } else if ((eleccion == "tijeras" && 
+   * this.eleccionesDis[eleccionM] == "piedra") ||
+   *       (eleccion == "papel" && 
+   * this.eleccionesDis[eleccionM] == "tijeras")
+   *       || (eleccion == "piedra" && this.
+   * eleccionesDis[eleccionM] == "papel")) {
+   *         this.texto3 = "PIERDES";
+   *  }else {
+   *   this.texto3 = "EMPATE";
+   * }
+   * }
+    */
 
 }
