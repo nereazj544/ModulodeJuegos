96ee589091b7256a147ca55d11eb219fd0b6d71e
nereazj544 <nzjvirtual@gmail.com>
2024-03-06 03:22:18 +0100

    Juego Boggle completo (solo falta modificar la apariencia de este) + Acabar Portafolios.

--- src/app/componentes/boggle/boggle.component.ts
+++ src/app/componentes/boggle/boggle.component.ts
@@ -1,79 +1,114 @@
 import { Component, OnInit } from '@angular/core';
 import { stCubes } from './cubes';
-import { dicionario } from './dicionario';
+
 
 @Component({
   selector: 'app-boggle',
   templateUrl: './boggle.component.html',
   styleUrls: ['./boggle.component.css']
 })
-export class BoggleComponent implements OnInit{
-  protected Size: number = 4; //!esto definira el tamaño 
+export class BoggleComponent implements OnInit {
+
   protected cubes: string[] = [];
-  protected letras: string[] = [];
-  protected palabrasEncontradas: string[] = [];
+  protected letters: string[] = [];
 
-  protected SelecionarPalabra: string[]=[];
-  protected LastSelecionCelda: [number, number] | null = null;
-  protected SelecionarCeldas=new Set<string>();
-  
-  starGame() {
-    this.palabrasEncontradas = [];
-    this.SelecionarPalabra = [];
-    this.SelecionarCeldas = new Set<string>();
+  protected wordsFound: string [] = [];
 
-
-    this.cubes = stCubes.concat();
-    shuffle(this.cubes);
-    this.letras = this.cubes.map(c => c[Math.round(Math.random() * 6)]);
-  }
-
-  protected selecionarLetra(row: number, col: number){
-    if(!this.isCell(row, col)){
-      return;
-    }
-
-    if(this.SelecionarCeldas.has(`${row}, ${col}`)){
-      return;
-    }
-
-    this.SelecionarPalabra.push(this.letras[row * 4 + col])
-    this.SelecionarCeldas.add(`${row}, ${col}`);
-    this.LastSelecionCelda = [row, col];
-}
-
-protected isCell(row: number, col: number){
-  return !this.LastSelecionCelda ||
-  (Math.abs(row - this.LastSelecionCelda[0]) <= 1 && Math.abs(col - this.LastSelecionCelda[1]) <= 1);
-}
-
-  protected puntuaje() {
-
-  }
-
-
-  cPalabra() {
-    const palabra = this.SelecionarPalabra.join('').toLowerCase();
-    if(dicionario.includes(palabra)){
-      this.palabrasEncontradas.push(palabra);
-    }
-    this.SelecionarCeldas = new Set();
-    this.SelecionarPalabra
-  }
+  //! Palabras
+  protected letterSelected: string [] = [];
+  protected selectedCells = new Set<string>();
+  protected selectedCellStack: [number, number][] = [];
 
   ngOnInit() {
     this.starGame();
   }
 
+  //! Metodo para selecionar las palabras (no funcina)
+  protected selectLetter(row: number, col: number) {
+
+    if (!this.isCellSectable(row, col)) {
+      return;
+    }
+    if (this.selectedCells.has(`${row}, ${col}`)) {
+      if (this.isLastSelectedCell(row, col)) {
+        this.letterSelected.pop()
+        this.selectedCellStack.shift();
+        this.selectedCells.delete(`${row}, ${col}`);
+        return;
+      }
+    }
+    this.selectedCells.add(`${row}, ${col}`);
+    this.letterSelected.push(this.letters[row * 4 + col]);
+    this.selectedCellStack.unshift([row, col]);
+  }
+
+  protected isCellSectable(row: number, col: number){
+    const lastSelected = this.selectedCellStack[0];
+    
+    if(!this.selectedCellStack.length){
+      return true;
+    }
+
+    if (!this.selectedCells.has(`${row}, ${col}`)) {
+      return Math.abs(row - lastSelected[0]) <= 1 && Math.abs(col - lastSelected[1]) <= 1;
+    }else{
+      return row === lastSelected[0] && col === lastSelected[0];
+    }
+  }
+
+  protected isLastSelectedCell(row: number, col: number){
+    return this.selectedCellStack.length &&
+    row == this.selectedCellStack[0][0] && col == this.selectedCellStack[0][1]
+  }
+
+  starGame() {
+    // this.wordsFound = [];
+    // this.letterSelected = [];
+    // this.selectedCells = new Set<string>();
+    this.restSelection();
+
+    this.cubes = stCubes.concat();
+    shuffle(this.cubes);
+    this.letters = this.cubes.map(c => c[Math.floor(Math.random() * 6)]);
+    //! si se deja como: Math.round 
+    //! Esto es para que se muestren todas las letras de los cubos.
+  }
+
+
+
+
+
+  //!Puntuaje
+  protected score() {
+    return this.wordsFound.map(w => w.length)
+  }
+
+
+  //! Palabra encontrtada
+  commitWord() {
+    const word = this.letterSelected.join('').toLowerCase();
+    // if (dicionary.includes(word)) {
+      this.wordsFound.push(word);
+    // }
+
+    this.restSelection();
+  }
+  
+  protected restSelection() {
+    this.selectedCellStack = [];
+    this.letterSelected = [];
+    this.selectedCells = new Set<string>();
+  }
   
 
-  // ! Fin del la clase  
+
+
+
 }
 
 function shuffle<T>(items: T[]): void {
-  for (let i = 0; i < items.length - 2; i++) {
+  for (let i = 0; i < items.length - 1; i++) {
     const swapIndex = Math.floor(Math.random() * (items.length - i));
     [items[i], items[swapIndex]] = [items[swapIndex], items[i]];
-
   }
 }
--- src/app/componentes/boggle/cubes.ts
+++ src/app/componentes/boggle/cubes.ts
@@ -1,18 +1,41 @@
 export const stCubes: string[] = [
+    'AACIOT', 
     'AAEEGN',
+    'ABILTY', 
     'ABBJOO',
+    'ABJMOQu', 
     'ACHOPS',
+    'ACDEMP', 
     'AFFKPS',
+    'ACELRS', 
     'AOOTTW',
+    'ADENVZ', 
     'CIMOTU',
+    'AHMORS', 
     'DEILRX',
+    'BIFORX', 
     'DELRVY',
+    'DENOSW', 
     'DISTTY',
+    'DKNOTU', 
     'EEGHNW',
+    'EEFHIY', 
     'EEINSU',
+    'EGKLUY', 
     'EHRTVW',
+    'EGINTV', 
     'EIOSST',
+    'EHINPS', 
     'ELRTTY',
+    'ELPSTU', 
     'HIMNUQu',
-    'HLNNRZ'
-];
+    'GILRUW', 
+    'HLNNRZ',
+    ];
+    
+    
+    //!Se usan los cubos nuevos y viejos.
+    /*
+    ! URL de referencia:
+    * https://www.bananagrammer.com/2013/10/the-boggle-cube-redesign-and-its-effect.html
+    */
