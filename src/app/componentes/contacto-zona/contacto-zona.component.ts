51ea905de8b266b7de7a0a22d79b06b09511bd9b
nereazj544 <nzjvirtual@gmail.com>
2024-03-10 00:03:28 +0100

    Implementacion de @Input y @Output

--- src/app/componentes/contacto-zona/contacto-zona.component.ts
+++ src/app/componentes/contacto-zona/contacto-zona.component.ts
@@ -6,13 +6,22 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./contacto-zona.component.css']
 })
 export class ContactoZonaComponent {
-  @Input () datosFormulario: any;
-  @Output() enviarFormulario = new EventEmitter;
+  
+  //* Con ! se indica que no puede ser null o undefined. Por lo que siempre se debe de tener un valor.
+  
+  @Input() nombre!:string; 
+  @Input() email!:string;
+  @Output() enviarFormulario = new EventEmitter<any>();
+
+//  nombre: string="";
+//  email: string = "";
+mensaje: string = "";
 
   constructor(){}
 
   enviar(){
-    this.enviarFormulario.emit(this.datosFormulario);
+    this.enviarFormulario.emit({nombre: this.nombre, email: this.email, mensaje: this.mensaje});
+    
   }
 
 }
--- src/app/componentes/portafolios/portafolios.component.ts
+++ src/app/componentes/portafolios/portafolios.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
 })
 export class PortafoliosComponent {
 
+  nombre: string = '';
+  email: string = '';
+  enviarDatos(datos: { nombre: string, email: string, mensaje: string}){
+    console.log("Formulario eviado", datos);
+    
+    //!Con esto lo que hara es que se muestre solo por la consola del navegador
+  }
 
 }
